703b44a9a4bebc56ce4d91709e2d84792f3fc668
felip-SN <felipenascimentooudri@gmail.com>
2025-02-25 22:10:37 -0300

    Terminado a estilização

--- curriculo/src/app/main/main.component.ts
+++ curriculo/src/app/main/main.component.ts
@@ -6,37 +6,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
-export class MainComponent implements OnInit {
+export class MainComponent {
 
-  items: {title: string; content: string; active?: boolean}[] = [
-    {
-      title: 'Item 1',
-      content: 'Content 1',
-    },
-    {
-      title: 'Item 2',
-      content: 'Content 2',
-    },
-    {
-      title: 'Item 3',
-      content: 'Content 3',
-    },
-    {
-      title: 'Item 4',
-      content: 'Content 4',
-    },
-  ];
-
-  ngOnInit(): void {}
-  constructor(){}
-
-  itemClicked(i: number){
-    this.items = this.items.map((item) => {
-      item.active = false;
-
-      return item;
-    });
-
-    this.items[i].active = true;
-  }
 }
